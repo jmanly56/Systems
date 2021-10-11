12c3ef5a31ad48bfbbc2c34d8f2ee7423d364d2e
Jonny <faane784@gmail.com>
2021-10-11 10:38:56 -0700

    Initial commit.Working window.

---
+++ main.cpp
@@ -0,0 +1,37 @@
+#include <SDL.h>
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+        SDL_Window *window = nullptr;
+        SDL_Renderer *renderer = nullptr;
+        SDL_Event event;
+
+        if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+                printf("Failed to init SDL.\n");
+        }
+
+        window = SDL_CreateWindow("Systems", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 1280,
+                                  720, NULL);
+
+        if (!window) {
+                printf("Failed to create window.\n");
+        }
+
+        SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+
+        renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+        while (1) {
+                while (SDL_PollEvent(&event)) {
+                        switch (event.type) {
+                        case SDL_QUIT:
+                                exit(0);
+                                break;
+
+                        default:
+                                break;
+                        }
+                }
+        }
+        return 0;
+}
