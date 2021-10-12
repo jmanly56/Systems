9898094a5924f5625633ba832a7b0ba1005a59f2
Havear <havearloraine@gmail.com>
2021-10-12 09:01:41 -0400

    Finished temperature color code. Minor constructor refactor.

--- star.cpp
+++ star.cpp
@@ -10,16 +10,16 @@ Star::Star()
     {
         this->mass = nd(generator);
     }
-    luminosity = calcLuminosity(mass);
-    radius = calcRadius(mass);
     temperature = pow((luminosity / pow((radius / solarRadiusInKM), 2.0)), 0.25) * 5777;
     mass = mass * solarMassInKG;
+    Star(radius, mass, 0);
 
 }
 
 Star::Star(double radius, double mass, double orbitalRadius)
 {
-
+    luminosity = calcLuminosity(mass);
+    radius = calcRadius(mass);
 }
 
 double Star::getSolarMass()
@@ -29,5 +29,54 @@ double Star::getSolarMass()
 
 SDL_Color Star::getTemperatureColor()
 {
+    SDL_Color color;
     int temp = static_cast<int>(temperature) / 100;
+    int red = 0;
+    int green = 0;
+    int blue = 0;
+
+    //Calc Red
+    if (temp <=66)
+    {
+        red = 255;
+    }
+    else
+    {
+        red = temp - 60;
+        red = 329.698727446 * pow(red, -0.1332047592);
+        if (red < 0) { red = 0; }
+        if (red > 255) { red = 255; }
+    }
+    color.r = red;
+    //Calc Green
+    if (temp <= 66)
+    {
+        green = temp;
+        green = 99.4708025861 * log(green) - 161.1195681661;
+        if (green < 0) { green = 0; }
+        if (green > 255) { green = 255; }
+    }
+    else
+    {
+        green = temp - 60;
+        green = 288.1221695283 * pow(green, -0.0755148492);
+        if (green < 0) { green = 0; }
+        if (green > 255) { green = 255; }
+    }
+    color.g = green;
+    //Calc Blue
+    if (temp >= 66)
+    {
+        blue = 255;
+    }
+    else
+    {
+        blue = temp - 10;
+        blue = 138.5177312231 * log(blue) - 305.0447927307;
+        if (blue < 0) { blue = 0; }
+        if (blue > 255) { blue = 255; }
+    }
+    color.b = blue;
+    color.a = 255;
+    return color;
 }
